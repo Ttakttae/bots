e15116736f4072bc8bee0a13e837bbf414ad3e52
박현우 <65493966+hyunwoo6321@users.noreply.github.com>
2022-05-26 13:57:37 +0000

    update

---
+++ Server_Manage_Bot/main.py
@@ -0,0 +1,24 @@
+import asyncio, discord, subprocess, os
+
+from discord_slash import SlashCommand, SlashContext
+from dotenv import load_dotenv
+from discord_slash.utils.manage_commands import create_option
+
+try:
+    load_dotenv("./token.env")
+    token = os.getenv('token')
+except:
+    token = os.environ['token']
+
+print(token)
+intents = discord.Intents.default()
+client = discord.Client(intents=intents)
+slash = SlashCommand(client, sync_commands=True)
+
+@slash.slash(name="specification", description="컴퓨터의 사양을 보여줍니다.")
+async def specification(message):
+    specification = str(subprocess.check_output('ls')).encode("UTF-8")
+    print(specification)
+    await message.send(specification)
+
+client.run(token)
