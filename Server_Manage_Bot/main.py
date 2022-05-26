98949c6b3fce5e59fcde135c8baeef7764491467
박현우 <65493966+hyunwoo6321@users.noreply.github.com>
2022-05-26 23:43:52 +0900

    run명령어 추가(테스트 안함)

--- Server_Manage_Bot/main.py
+++ Server_Manage_Bot/main.py
@@ -21,4 +21,24 @@ async def specification(message):
     print(specification)
     await message.send(specification)
 
+@slash.slash(name="run", description="서버의 터미널에 원격으로 명령을 실행시킵니다", options=[create_option(name="command", description="실행 할 명령", option_type=3, required=True)])
+async def run(message, command: str):
+    if message.channel.id == 968088658093690920:
+        commands = command.split(" ")
+        result = subprocess.check_output(commands, shell=True).decode('ascii')
+        print(result)
+        if result > 2000:
+            await message.send(result[0:2000])
+            await message.send("메시지가 너무 길어 2000자만 보냅니다.")
+        else:
+            await message.send(result)
+    else:
+        await message.send("서버 관리 채널에서만 사용할 수 있는 명령어입니다.")
+
+@client.event
+async def on_ready():
+    print('봇시작')
+    await client.change_presence(activity=discord.Activity(type=discord.ActivityType.playing, name="서버 관리")) #상태설정
+    discord.Permissions.use_slash_commands = True
+
 client.run(token)
