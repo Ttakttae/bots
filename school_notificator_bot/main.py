7f1dff85e2d59a607313b6841b84185cc1cbb041
박현우 <65493966+hyunwoo6321@users.noreply.github.com>
2022-05-29 16:36:51 +0900

    채널 등록 명령어만 추가

--- school_notificator_bot/main.py
+++ school_notificator_bot/main.py
@@ -1,4 +1,4 @@
-import asyncio, discord
+import asyncio, discord, json, os
 
 from discord_slash import SlashCommand, SlashContext
 from dotenv import load_dotenv
@@ -15,18 +15,14 @@ intents = discord.Intents.default()
 client = discord.Client(intents=intents)
 slash = SlashCommand(client, sync_commands=True)
 
-@slash.slash(name="register_channel", description="알림을 전송할 채널을 등록합니다", options=[create_option(name="command", description="실행 할 명령", option_type=3, required=True)])
-async def run(message, command: str):
-    if message.channel.id == 968088658093690920:
-        result = subprocess.check_output(command, shell=True).decode('ascii')
-        print(result)
-        if len(result) > 2000:
-            await message.send(result[0:2000])
-            await message.send("메시지가 너무 길어 2000자만 보냅니다.")
-        else:
-            await message.send(result)
-    else:
-        await message.send("서버 관리 채널에서만 사용할 수 있는 명령어입니다.")
+@slash.slash(name="register_channel", description="알림을 전송할 채널을 등록합니다", options=[create_option(name="channel", description="전송할 채널", option_type=7, required=True)])
+async def run(message, channel: str):
+    with open("channels.json", 'r', encoding='UTF-8') as f:
+        channels = dict(json.load(f))
+    channels["channels"].append(channel.id)
+    with open("channels.json", 'w', encoding='UTF-8') as f:
+        json.dump(channels, f)
+    await message.send("채널 등록이 완료되었습니다")
 
 @client.event
 async def on_ready():
